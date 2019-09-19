155eeb2bc4dbd16b0fddc75c4b4b39c3e59fb682
Steve Owens <sd.owens@outlook.com>
2019-09-18 22:09:44 -0700

    modified app.js to connect to MongoDB Atlas

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ const commentRoutes = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+mongoose.connect(
+	"mongodb+srv://<admin>:<SilverFreak>@cluster0-ssxqb.mongodb.net/yelp_camp?retryWrites=true&w=majority"
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 // eslint-disable-next-line no-undef
